0a9e6624e4fcb22b63228709f2c0bb67eccb52dd
rencyngina <rencyngina@gmail.com>
2023-03-28 21:36:44 +0000

    trying out the code

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,7 +1,9 @@
-const getListStudents = () => [
-	{ id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-	{ id: 2, firstName: 'James', location: 'Columbia' },
-	{ id: 5, firstName: 'Serena', location: 'San Francisco' }
-]
+function getListStudents() {
+	var object1 = {id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+	var object2 = {id: 2, firstName: 'James', location: 'Columbia' },
+	var object3 = {id: 5, firstName: 'Serena', location: 'San Francisco' }
 
-export default getListStudents;
+	var arr = [obj1, obj2, obj3];
+
+	return arr;
+}
