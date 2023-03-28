7c6bb0106972adc2f03feed2d0b387375ea8459c
rencyngina <rencyngina@gmail.com>
2023-03-28 21:49:38 +0000

    main code

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,7 +1,7 @@
 function getListStudents() {
-	var object1 = {id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-	var object2 = {id: 2, firstName: 'James', location: 'Columbia' },
-	var object3 = {id: 5, firstName: 'Serena', location: 'San Francisco' }
+	var object1 = {id: 1, firstName: 'Guillaume', location: 'San Francisco' };
+	var object2 = {id: 2, firstName: 'James', location: 'Columbia' };
+	var object3 = {id: 5, firstName: 'Serena', location: 'San Francisco' };
 
 	var arr = [obj1, obj2, obj3];
 
